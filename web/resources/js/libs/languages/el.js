9d8253181d235b05a01375efc72294db5e2023ba
stefano <s.kouroupis@gmail.com>
2015-10-16 15:06:56 +0300

    Show/Hide Layers Menu

--- web/resources/js/libs/languages/el.js
+++ web/resources/js/libs/languages/el.js
@@ -1,3 +1,5 @@
 var LClearResults = "Καθαρισμός Αποτελεσμάτων";
 var LScale = "Κλίμακα";
 var LZoomIn = "Μεγέθύνση";
+var LShowHideLayers = "Εμφάνιση/Απόκρυψη Επιπέδων";
+var LHideAll = "Απόκρυψη Όλων των Επιπέδων";
--- web/resources/js/libs/languages/en-US.js
+++ web/resources/js/libs/languages/en-US.js
@@ -1,3 +1,5 @@
 var LClearResults = "Clear Results";
 var LScale = "Scale";
 var LZoomIn = "Zoom In";
+var LShowHideLayers = "Show/Hide Layers";
+var LHideAll = "Hide All Layers";
