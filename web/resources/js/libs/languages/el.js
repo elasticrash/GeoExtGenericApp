9df2e3633e1e3d2632307da2e2c9b6544ebc26fd
stefano <s.kouroupis@gmail.com>
2015-10-16 19:15:44 +0300

    more comments/more entries in language files

--- web/resources/js/libs/languages/el.js
+++ web/resources/js/libs/languages/el.js
@@ -3,3 +3,6 @@ var LScale = "Κλίμακα";
 var LZoomIn = "Μεγέθύνση";
 var LShowHideLayers = "Εμφάνιση/Απόκρυψη Επιπέδων";
 var LHideAll = "Απόκρυψη Όλων των Επιπέδων";
+var LNavigate = "Πλοήγηση";
+var LPrint = "Εκτύπωση";
+var LIdentify = "Αναγνώριση";
--- web/resources/js/libs/languages/en-US.js
+++ web/resources/js/libs/languages/en-US.js
@@ -3,3 +3,6 @@ var LScale = "Scale";
 var LZoomIn = "Zoom In";
 var LShowHideLayers = "Show/Hide Layers";
 var LHideAll = "Hide All Layers";
+var LNavigate = "Navigate";
+var LPrint = "Print";
+var LIdentify = "Identify";
