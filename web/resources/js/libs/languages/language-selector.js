79373a59779788c509110627e028e12c371e69f7
stefano kitano <S.Kouroupis@gmail.com>
2015-10-18 00:03:17 +0300

    removed Ext in dynamically loading language files cause due to async loading it didnt work on some browsers

--- web/resources/js/libs/languages/language-selector.js
+++ web/resources/js/libs/languages/language-selector.js
@@ -1,27 +1,43 @@
 /**
  * Created by stefanos on 16/10/2015.
  */
-if(language == "en-US")
-{
-    loadjsfile('resources/js/libs/languages/en-US.js');
-}
-if(language == "el")
-{
-    loadjsfile("resources/js/libs/languages/el.js");
-}
-//if no file language exists it will load english by default
-else
-{
-    loadjsfile('resources/js/libs/languages/en-US.js');
-}
+var language = window.navigator.userLanguage || window.navigator.language;
 
-function loadjsfile(url){
-    Ext.Loader.loadScript({
-        url: url,
-        scope: this,
-        onLoad: function() {
-        },
-        onError: function() {
+(function () {
+
+    function loadScript(url) {
+
+        var script = document.createElement("script")
+        script.type = "text/javascript";
+
+        if (script.readyState) { //IE
+            script.onreadystatechange = function () {
+                if (script.readyState == "loaded" || script.readyState == "complete") {
+                    script.onreadystatechange = null;
+                }
+            };
+        } else { //Others
+            script.onload = function () {
+            };
         }
-    });
-}
+
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);
+    }
+
+    if(language == "en-US")
+    {
+        loadScript('resources/js/libs/languages/en-US.js');
+    }
+    if(language == "el")
+    {
+        loadScript("resources/js/libs/languages/el.js");
+    }
+    //if no file language exists it will load english by default
+    else
+    {
+        loadScript('resources/js/libs/languages/en-US.js');
+    }
+
+
+})();
