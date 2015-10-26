a206f6cd7d45f00258150a0d34b170e282b304c8
stefano kitano <S.Kouroupis@gmail.com>
2015-10-26 22:07:45 +0200

    layer visibility moved to options

--- web/resources/js/libs/Options.js
+++ web/resources/js/libs/Options.js
@@ -1,46 +1,53 @@
 /**
  * Created by tougo on 26/10/15.
  */
+var optionform=null;
 function OpenOptionForm(tree, layers) {
 
     var wsize = Ext.getBody().getViewSize();
 
-    var optionform = new Ext.Window({
-        title: 'Options',
-        width: wsize.width/2,
-        height: wsize.height/2,
-        autoScroll: true,
-        items: [{
-            id: 'layerfieldset',
-            xtype: 'fieldset',
-            title: "Layer Visibility",
-            collapsible: true,
-            autoHeight: true,
-            margin: 10,
-            items:[{
-                xtype: 'checkboxgroup',
-                columns: 3,
-                items: layers
-            }]
-        },
-        {
-            xtype: 'button',
-            text: LHideAll,
-            handler: function() {
-                var treeNode = tree.getRootNode();
-                while(treeNode.childNodes[0].firstChild) {
-                    treeNode.childNodes[0].removeChild(treeNode.childNodes[0].firstChild);
-                }
-                var tmenu = Ext.getCmp('layerfieldset');
-                for(var i =1; i < tmenu.items.items[0].items.length; i++)
+    if(optionform==null) {
+        optionform = new Ext.Window({
+            title: 'Options',
+            width: wsize.width / 2,
+            height: wsize.height / 2,
+            autoScroll: true,
+            closeAction: 'hide',
+            items: [{
+                id: 'layerfieldset',
+                xtype: 'fieldset',
+                title: "Layer Visibility",
+                collapsible: true,
+                autoHeight: true,
+                margin: 10,
+                items: [{
+                    xtype: 'checkboxgroup',
+                    columns: 3,
+                    items: layers
+                }]
+            },
                 {
-                    Ext.getCmp(tmenu.items.items[0].items.items[i].id).checked =false;
-                    Ext.getCmp(tmenu.items.items[0].items.items[i].id).removeCls('x-form-cb-checked x-form-dirty');
-                    Ext.getCmp(tmenu.items.items[0].items.items[i].id).checkChange();
-                }
-            }
-         }]
-    });
+                    xtype: 'button',
+                    text: LHideAll,
+                    handler: function () {
+                        var treeNode = tree.getRootNode();
+                        while (treeNode.childNodes[0].firstChild) {
+                            treeNode.childNodes[0].removeChild(treeNode.childNodes[0].firstChild);
+                        }
+                        var tmenu = Ext.getCmp('layerfieldset');
+                        for (var i = 1; i < tmenu.items.items[0].items.length; i++) {
+                            Ext.getCmp(tmenu.items.items[0].items.items[i].id).checked = false;
+                            Ext.getCmp(tmenu.items.items[0].items.items[i].id).removeCls('x-form-cb-checked x-form-dirty');
+                            Ext.getCmp(tmenu.items.items[0].items.items[i].id).checkChange();
+                        }
+                    }
+                }]
+        });
+    }
+    if(optionform!=null)
+    {
+        optionform.show();
+    }
 
     function onItemCheck(item, checked) {
         var treeNode = tree.getRootNode();
@@ -64,5 +71,4 @@ function OpenOptionForm(tree, layers) {
             });
         }
     }
-    optionform.show();
 }
