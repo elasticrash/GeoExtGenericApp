96a616e03c53d0a62ebf36ac66f6ea8cd09004d1
stefano <s.kouroupis@gmail.com>
2015-11-09 00:35:24 +0200

    Update Config.js

--- web/resources/js/libs/Config.js
+++ web/resources/js/libs/Config.js
@@ -5,11 +5,10 @@ var geoserverWfsDefaults= {
         wfsFeatureNS: "http://demo-greece/Greek",
         wfsVersion: "1.0.0",
         wfsSrsName: "EPSG:2100",
-        nsAlias: 'ELPHO',
-        wfsCapabilities: '<GetCapabilities service="WFS" version="1.0.0" xmlns="http://www.opengis.net/wfs" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.0.0/WFS-basic.xsd"/>'
-}
+        nsAlias: 'ELPHO'
+};
 var geoserverWmsDefaults= {
     wmsUrl: geoserver+"geoserver/ELPHO/wms?",
     wmsSrsName: "EPSG:2100",
     nsAlias: 'ELPHO'
-}
+};
